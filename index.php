1a6a5052ce6b294315b69a71a1d6adb2810badae
muhibahmedaljunied <muhibalmuhib10@gmail.com>
2023-11-20 11:51:58 +0300

    update index.php into index1.php

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo 'hi';
-
-// ['server', 'username', 'password', 'port', 'protocol', 'local-dir', 'server-dir', 'state-name', 'dry-run', 'dangerous-clean-slate', 'exclude', 'log-level', 'security', 'timeout']
